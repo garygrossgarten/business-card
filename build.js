e674e6a63cffeb4217353d409634eab7b97cc713
Gary Großgarten <gary.grossgarten@gmail.com>
2018-12-28 02:49:36 +0100

    customizations and simplifications 🍕

--- build.js
+++ build.js
@@ -5,51 +5,54 @@ const chalk = require('chalk')
 const boxen = require('boxen')
 const fs = require('fs')
 const path = require('path')
+const terminalLink = require('terminal-link');
+const terminalImage = require('terminal-image');
 
-// Define options for Boxen
-const options = {
-  padding: 2,
-  margin: 2,
-  borderStyle: 'round'
-}
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Gary Großgarten'),
-  handle: chalk.white('garygrossgarten'),
-  work: chalk.white('Software Developer Freelancer'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.white('garygrossgarten'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.white('garygrossgarten'),
-  github: chalk.gray('https://github.com/') + chalk.white('garygrossgarten'),
-  web: chalk.cyan('https://garygrossgarten.de'),
-  npx: chalk.red('npx') + ' ' + chalk.white('@garygrossgarten/me'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
+  name: 'Gary Großgarten',
+  handle: 'garygrossgarten',
+  work: 'Software Developer',
+  web: 'https://garygrossgarten.de',
+  twitter: 'https://twitter.com/garygrossgarten',
+  GitHub: 'https://github.com/garygrossgarten',
+  npm: 'https://npmjs.com/~garygrossgarten',
+  card: '@garygrossgarten/me'
 }
 
-// Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} / ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const options = {
+  fallback: (text, url) => {
+    return url;
+  }
+}
 
-// Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+const output = chalk.white(`                                     
+,gggg,gg    ,gggg,gg   ,gggggg,  gg     gg 
+dP"  "Y8I   dP"  "Y8I   dP""""8I  I8     8I 
+i8'    ,8I  i8'    ,8I  ,8'    8I  I8,   ,8I 
+,d8,   ,d8I ,d8,   ,d8b,,dP     Y8,,d8b, ,d8I 
+ "Y8888P"888P"Y8888P"\`Y88P       \`8P""Y88P"8
+     ,d8I'                             ,d8I'
+   ,dP'8I    ${chalk.blueBright(data.name)}          ,dP'8I 
+   ,8"  8I   ${chalk.gray(data.work)}      ,8"  8I 
+   I8   8I                           I8   8I 
+  \`8, ,8I                           \`8, ,8I 
+   \`Y8P"                             \`Y8P"  
+${'-'.repeat(46)}
+${chalk.hex('#1dcaff')('Twitter:')} ${terminalLink('@' + data.handle, data.twitter, options)}   
+${chalk.blueBright('    Web:')} ${terminalLink('garygrossgarten.de', data.web, options)} 
+${chalk.hex('#211F1F')(' GitHub:')} ${terminalLink(data.handle, data.GitHub, options)}   
+${chalk.hex('#CC3534')('    npm:')} ${terminalLink('~' + data.handle, data.npm, options)}   
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.white(boxen(output, options)))
+          ${chalk.hex('#CC3534')('npx')} ${data.card}   
+`)
+
+// terminalImage.file('garygrossgarten.jpeg')
+// .then((image)=>{
+//   const o = image + '\n' + output;
+//   fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.gray(boxen(o, { padding: 1, margin: 2, borderStyle: 'round' })));
+// }).catch(err => console.log(err.text));
+
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.gray(boxen(output, { padding: 1, margin: 1, borderStyle: 'round' })));
+
